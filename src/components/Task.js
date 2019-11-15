0a20a5613e25d0e566cdb83d768267bf7a5a7b1b
Yani Rodriguez <yani010712@gmail.com>
2019-11-15 13:37:26 -0500

    Adding for and addTask and deleteTask methods

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Task extends Component {
+
+    StyleCompleted() {
+        return {
+            fontsSize: '20px',
+            color: this.props.task.done ? 'gray' : 'black',
+            textDecoration: this.props.task.done ? 'line-through':'none',
+        }
+    }
+
+
     render(){
         const { task } = this.props;
-
-        return <div>
+        return <div style={this.StyleCompleted()}>
            {task.title} - 
            {task.description} - 
            {task.done} - 
            {task.id}
-            <input type="checkbox"/>
-            <button>x</button>
+            <input type="checkbox" onChange={this.props.checkDone.bind(this, task.id)}/>
+            <button style={btnDelete} onClick={this.props.deleteTask.bind(this, task.id)}>
+                x
+            </button>
         </div>
     }
 
+}
+
+Task.props = {
+    task: PropTypes.object.isRequired
+}
+
+
+const btnDelete = {
+    fontSize: '18px',
+    background: '#ea2027',
+    color: '#fff',
+    border: 'none',
+    padding: '10px 15px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+     
 }
 
 export default Task;
---
+++ src/components/TaskForm.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+export default class TaskForm extends Component {
+
+    state = {
+        title: '',
+        description: ''
+    }
+    onSubmit = e => {
+        this.props.addTask(this.state.title, this.state.description)
+        e.prevenDefault();
+        
+        
+    }
+
+    onChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+                <input 
+                    type="text" 
+                    name="title"
+                    placeholder="Write a Task" 
+                    onChange={this.onChange} 
+                    value={this.state.title} />
+                <br/>
+                <br/>
+                <textarea 
+                    name="description"
+                    placeholder="Write a Description" 
+                    onChange={this.onChange} 
+                    value={this.state.description}></textarea>
+                <button type="button" onClick={this.props.addTask.bind(this, this.state.title, this.state.description )}>
+                    Send
+                </button>
+            </form>
+        )
+
+
+    }
+    
+}
