98bb7f7c1039e3ed56bb03781c98f07fa29be8fc
Yani Rodriguez <yani010712@gmail.com>
2019-11-15 21:18:54 -0500

    Fetching data and adding react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component} from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
 
 import tasks from './sample/tasks.json';
 
 import Tasks from './components/Tasks';
 import TaskForm from './components/TaskForm';
+import Posts from './components/Posts';
 
 
 class App extends Component {
@@ -42,12 +44,27 @@ class App extends Component {
 
   render() {
     return <div>
-      <TaskForm  addTask={this.addTask}/>
-      <Tasks 
-        tasks={this.state.tasks}
-        deleteTask={this.deleteTask}
-        checkDone={this.checkDone}
-      />
+      <Router>
+        <Link to="/">Home</Link>
+        <br/>
+        <Link to="/posts">Posts</Link>
+        <Route exact path="/" render={() => {
+          return <div>
+            <TaskForm  addTask={this.addTask}/>
+            <Tasks 
+              tasks={this.state.tasks}
+              deleteTask={this.deleteTask}
+              checkDone={this.checkDone}
+              />
+          </div>
+        }}>
+        </Route>
+        <Route path="/posts" component={Posts} />
+
+       
+      </Router>
+      
+      <Posts />
     </div>
   }
 
